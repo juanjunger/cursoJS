34d079c72bcc4178111fc48167de2a3459a2ab10
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:41:34 -0300

    feat: add aula-12 folder

---
+++ aula-12/js/script.js
@@ -0,0 +1,7 @@
+/*
+** alert, confirm e promp são métodos do objeto window presente nos navegadores
+*/
+
+alert('Olá mundo!');
+confirm('Deseja realmente fechar esta janela?');
+prompt('Digite o seu nome: ');
