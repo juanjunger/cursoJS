e71351cd8fa08f1d6fdd893ca50a7807ee39efd3
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:42:34 -0300

    feat: add aula-21 folder

---
+++ aula-21/index.js
@@ -0,0 +1,20 @@
+/*
+** Operadores de comparação
+** 
+** Retorna true ou false de acordo com uma expressão
+**
+** > maior que
+** >= maior que ou igual a
+** < menor que
+** <= menor que ou igual a
+** != diferente (valor)
+** !== diferente estrito (valor e tipo)
+** == igualdade (valor)
+** === igualdade estrita (valor e tipo)
+*/
+
+console.log(10 == '10') // checa se o valor é igual e não o tipo da variável = true
+console.log(10 === '10') // checa se o valor e o tipo das variáveis são iguais = false
+
+console.log(10 != '10') // checa se o valor é diferente e não o tipo da variável = false
+console.log(10 !== '10') // checa se o valor e o tipo das variáveis são diferentes = true
