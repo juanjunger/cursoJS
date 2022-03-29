7bb18855d8c689fbbff962e7c534111055f2c592
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:42:39 -0300

    feat: add aula-22 folder

---
+++ aula-22/index.js
@@ -0,0 +1,14 @@
+/*
+** Operadores lógicos
+** 
+** Compara o resultado de expressões lógicas ou comparativas
+** Tabela verdade
+** 
+** && -> AND -> E
+** || -> OR  -> OU
+** !! -> NOT NOT -> NÃO NÃO -> Negação da negação = retorna pro valor original
+** !  -> NOT -> NÃO
+**
+*/
+
+
