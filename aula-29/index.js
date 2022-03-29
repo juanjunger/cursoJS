654c9c3bb0dcbea418a5b7d3bfa12f7e34d0ec06
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:43:28 -0300

    feat: add aula-29 folder

---
+++ aula-29/index.js
@@ -0,0 +1,11 @@
+/*
+** Não podemos redeclarar a variável com a mesma nomenclatura usando o let no mesmo escopo
+** let tem escopo de bloco
+** var só tem escopo de função
+** 
+** clojure
+** hoisting
+*/
+
+let name1 = 'Juan';
+var name2 = 'Carlos'
