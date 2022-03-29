544564ffd1e3d59e6b5a6b7edb8d6d662e4c5911
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:40:30 -0300

    feat: add aula-04 folder

---
+++ aula-04/comments.js
@@ -0,0 +1,16 @@
+// Comentário em linha
+console.log("Linha 1"); // Aqui tem um comentário, que será ignorado na execução do código
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+console.log("Linha 5");
+
+
+/* Comentário em bloco */
+/* 
+** console.log("Linha 1");
+** console.log("Linha 2");
+** console.log("Linha 3");
+** console.log("Linha 4");
+** console.log("Linha 5"); 
+*/
