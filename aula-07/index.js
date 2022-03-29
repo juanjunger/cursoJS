cf010a0b5ea82a37c5516edd360bde6ab6b3fdba
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:40:52 -0300

    feat: add aula-07 folder

---
+++ aula-07/index.js
@@ -0,0 +1,14 @@
+/* 
+** Não podemos criar constantes com palavras reservadas da linguagem (let, console, if...)
+** Constantes precisam ter nomes significativos (boa prática)
+** Constantes não podem iniciar com números ou caracteres especiais na declaração
+** Constantes não podem conter espaços ou traços
+** Por convenção, utilizamos camelCase (boa prática)
+** Constantes são Case-sensitive (distinção entre maiúsculas e minúsculas)
+** Não podemos modificar o valor de uma constante, ou seja, é imutável
+** Não podemos redeclarar uma constante
+** Não podemos declarar uma constante sem um valor inicial
+*/
+
+const firstName = 'Juan';
+console.log(firstName);
