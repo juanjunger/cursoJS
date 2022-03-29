fcdceba5148b4d2e9cc58e925e97100203a97c33
juanjunger <juancarlo.h.junger@gmail.com>
2022-03-29 12:43:14 -0300

    feat: add aula-27 folder

---
+++ aula-27/index.js
@@ -0,0 +1,32 @@
+/*
+** Switch Case
+*/
+
+const date = new Date();
+const dayOfWeek = date.getDay();
+
+switch (dayOfWeek) {
+  case 0:
+    console.log('Domingo');
+    break;
+  case 1:
+    console.log('Segunda');
+    break;
+  case 2:
+    console.log('Terça');
+    break;
+  case 3:
+    console.log('Quarta');
+    break;
+  case 4:
+    console.log('Quinta');
+    break;
+  case 5:
+    console.log('Sexta');
+    break;
+  case 6:
+    console.log('Sábado');
+    break;
+  default:
+    break;
+}
